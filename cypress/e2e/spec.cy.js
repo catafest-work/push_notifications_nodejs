e67852c2938a09ed86b84a9c22a1a3c3c18a6d78
Catalin George Festila <catafets76@gmail.com>
2024-12-16 14:58:20 +0200

    update file cypress , screenshot

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
-    //cy.visit('http://localhost:3000')
     cy.request('http://localhost:3001')
   })
 })
+
 describe('Notification System Tests', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3001')
-    // Add a small delay to ensure scripts are loaded
     cy.wait(1000)
   })
 
@@ -27,14 +26,12 @@ describe('Notification System Tests', () => {
         .should('eq', 200)
   })
 
-  // Test the Notification API availability
   it('should have Notification API available', () => {
     cy.window().then((win) => {
       expect(win.Notification).to.exist
     })
   })
 
-  // Test if notifications array exists in the source
   it('should contain notification messages in source', () => {
     cy.request('/notification_service.js')
         .its('body')
@@ -49,26 +46,29 @@ describe('Notification System Tests', () => {
     cy.wait(1000)
   })
 
-  // Button Tests
   it('should interact with notification buttons', () => {
-    // Test enable notifications button
-    cy.get('button').contains('Enable Notifications').should('be.visible')
-    cy.get('button').contains('Enable Notifications').click()
-
-    // Test show notification button
-    cy.get('button').contains('Show Test Notification').should('be.visible')
-    cy.get('button').contains('Show Test Notification').click()
+    // First verify button exists
+    cy.get('[data-test="enable-button"]').should('exist')
+    
+    // Then force click if needed
+    cy.get('[data-test="enable-button"]')
+      .invoke('show')
+      .click()
+  
+    cy.get('[data-test="show-button"]')
+      .invoke('show')
+      .click()
   })
 
-  // Notification Permission Tests
   it('should handle notification permissions', () => {
     cy.window().then((win) => {
       cy.stub(win.Notification, 'requestPermission').resolves('granted')
-      cy.get('button').contains('Enable Notifications').click()
+      cy.get('[data-test="enable-button"]')
+        .invoke('show')
+        .click({ force: true })
     })
   })
 
-  // Popup Notification Tests
   it('should create notification popup', () => {
     cy.window().then((win) => {
       cy.stub(win.Notification, 'permission').value('granted')
@@ -79,7 +79,7 @@ describe('Notification System Tests', () => {
             show: notificationStub
           })
 
-      cy.get('button').contains('Show Test Notification').click()
+      cy.get('[data-test="show-button"]').click()
 
       cy.get('@notificationStub').should('be.called')
       cy.get('@notificationStub').should((stub) => {
@@ -88,24 +88,81 @@ describe('Notification System Tests', () => {
       })
     })
   })
-
-  // Test notification content
   it('should display correct notification content', () => {
     cy.window().then((win) => {
       cy.stub(win.Notification, 'permission').value('granted')
-
+      
       const notifications = []
       cy.stub(win, 'Notification').callsFake((title, options) => {
         notifications.push({ title, options })
+        return { title, options }
       })
+  
+      cy.get('[data-test="show-button"]')
+        .invoke('show')
+        .click({ force: true })
+        
+      // Wait for notification to be created
+      cy.wrap(notifications, { timeout: 5000 }).should('have.length', 1)
+        .then(() => {
+          expect(notifications[0].title).to.equal('New Alert')
+        })
+    })
+  })
+  
+})
 
-      cy.get('button').contains('Show Test Notification').click()
+describe('Browser Layer Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
+  })
 
-      cy.wrap(notifications).should('have.length.at.least', 1)
-      cy.wrap(notifications[0]).should('deep.include', {
-        title: 'New Alert'
+  it('should display browser information div', () => {
+    cy.get('#browser-info').should('exist')
+  })
+
+  it('should show original and modified user agent', () => {
+    cy.get('#browser-info')
+      .find('p')
+      .should('have.length', 2)
+      .then(($p) => {
+        const originalUA = $p[0].textContent
+        const modifiedUA = $p[1].textContent
+        
+        expect(originalUA).to.include('Original User Agent')
+        expect(modifiedUA).to.include('Modified User Agent')
+        
+        if (originalUA.includes('Chrome')) {
+          expect(modifiedUA).to.include('Firefox')
+        }
       })
+  })
+
+  it('should have Firefox specific headers', () => {
+    cy.request('http://localhost:3001')
+      .its('headers')
+      .then((headers) => {
+        expect(headers).to.have.property('x-firefox-spoof', 'true')
+        expect(headers).to.have.property('accept-language', 'en-US,en;q=0.5')
+      })
+  })
+
+  it('should load notification service with correct headers', () => {
+    cy.request('http://localhost:3001/notification_service.js')
+      .then((response) => {
+        expect(response.headers['content-type']).to.equal('application/javascript')
+        expect(response.headers['x-firefox-spoof']).to.equal('true')
+        expect(response.status).to.equal(200)
+      })
+  })
+
+  it('should verify browser spoofing functionality', () => {
+    cy.window().then((win) => {
+      const userAgent = win.navigator.userAgent
+      if (userAgent.includes('Chrome')) {
+        cy.get('#browser-info')
+          .should('contain', 'Firefox')
+      }
     })
   })
 })
-
--- server_test_layer_apps.js
+++ server_test_layer_apps.js
@@ -1,25 +1,3 @@
-// const http = require('http');
-// const fs = require('fs');
-// const path = require('path');
-
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/notification_service.js') {
-//         fs.readFile('notification_service.js', (err, data) => {
-//             res.writeHead(200, {'Content-Type': 'application/javascript'});
-//             res.end(data);
-//         });
-//     } else {
-//         fs.readFile('index.html', (err, data) => {
-//             res.writeHead(200, {'Content-Type': 'text/html'});
-//             res.end(data);
-//         });
-//     }
-// });
-
-// server.listen(3001, () => {
-//     console.log('Server running at http://localhost:3001');
-// });
-
 // schimbare layer applicatie web
 const http = require('http');
 const fs = require('fs');
